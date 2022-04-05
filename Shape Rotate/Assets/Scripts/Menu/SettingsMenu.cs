6637f313e636b179ce7ced3f3420a28a8a684e10
James Johnstone <jjohnstone2520@gmail.com>
2022-04-05 13:32:29 +1200

    ~Added reset progress button ~Added notification toggle to settings ~Added sound volume slider to settings ~Added music volume slider to settings ~Added reset button to settings ~Created are you sure popup for resetting progress ~Added restore IAP purchases button in setting (doesn't work yet)

--- Shape Rotate/Assets/Scripts/Menu/SettingsMenu.cs
+++ Shape Rotate/Assets/Scripts/Menu/SettingsMenu.cs
@@ -1,21 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : Popup
 {
-    public override void OnStart()
-    {
-        base.OnStart();
-    }
+    public Slider musicSlider;
+    public Slider soundSlider;
+
+    public ResetGamePopup resetGamePopup;
 
     public override void OpenPopup()
     {
+        UpdateUI();
+
         base.OpenPopup();
     }
 
-    public override void ClosePopup()
+    private void UpdateUI()
     {
-        base.ClosePopup();
+        // Set up music slider
+        musicSlider.value = AudioManager.instance.GetMusicVolume();
+        musicSlider.maxValue = 1;
+        musicSlider.onValueChanged.AddListener(delegate { UpdateMusicSliderValue(); });
+
+        // Set up sfx slider
+        soundSlider.value = AudioManager.instance.GetSoundVolume();
+        soundSlider.maxValue = 1;
+        soundSlider.onValueChanged.AddListener(delegate { UpdateSoundSliderValue(); });
+    }
+
+    public void UpdateSoundSliderValue()
+    {
+        AudioManager.instance.SetSFXVolume(soundSlider.value);
+    }
+
+    public void UpdateMusicSliderValue()
+    {
+        AudioManager.instance.SetMusicVolume(musicSlider.value);
+    }
+
+
+    // UI Buttons
+    public void Button_RestorePurchases()
+    {
+        // Restores previous IAP purchases the player has made in the past
+    }
+    public void Button_Reset()
+    {
+        resetGamePopup.OpenPopup();
     }
 }
